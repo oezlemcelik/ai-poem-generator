19f16e2d9ae9e88e9a9bbc3bc008e374abe49c46
oezlemcelik <155397894+oezlemcelik@users.noreply.github.com>
2024-03-19 14:39:32 +0100

    Added the AI integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-function generatePoem (event){
-    event.preventDefault();
-
-
-    new Typewriter('#poem', {
-  strings: "Das Lieben bringt groß’ Freud’",
+function displayPoem (response){
+  console.log ("Poem generated");
+  new Typewriter('#poem', {
+  strings: response.data.answer,
   autoStart: true,
   delay: 1,
   cursor:"",
 });
+
+}
+
+
+function generatePoem (event){
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "bb0741b0aa475cabbe3bbdftd8oa9bfa";
+    let context = "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
+    let prompt = `User instructions: Generate a German poem about ${instructionsInput}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;    
+    console.log("Generating poem");
+    
+    axios.get(apiURL).then(displayPoem);
+
+
 }
 
 
